be8c1310e16c17a4ff2d20bc77d8c4355b31c296
VLAD EMINOVICI <veminovici@hotmail.com>
2025-03-21 16:37:13 +0800

    Revert "Refactor scale traits by removing validation traits and simplifying the Scale struct. Update the ScaleQuality trait to focus on defining scale properties. Remove associated tests for major and natural minor scales to streamline the codebase."
    This reverts commit fbefe9336791a0a39e00b11387b7a734d884f6d3.

--- src/scales/heptatonic/major.rs
+++ src/scales/heptatonic/major.rs
@@ -4,3 +4,15 @@ define_scale!(Major, [WHOLE, WHOLE, HALF, WHOLE, WHOLE, WHOLE, HALF]);
 
 pub use major::constants::*;
 pub use major::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::prelude::*;
+
+    #[test]
+    fn test_major_scale() {
+        let scale = major_scale(C4);
+        assert_eq!(scale.pitches(), &[C4, D4, E4, F4, G4, A4, B4, C5]);
+    }
+}
